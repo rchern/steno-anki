85e85b2cb008ba18b7932b802cda709fd9bf45ba
Rebecca Chernoff <rebecca@chernoff.com>
2021-07-02 21:57:10 -0500

    #4 - implement processing multiple decks

--- steno_anki/main.py
+++ steno_anki/main.py
@@ -1,35 +1,53 @@
-import fnmatch
 from argparse import ArgumentParser
+import fnmatch
 import os
+from pathlib import Path
 
 from anki import Anki
 
 args = ArgumentParser()
 args.add_argument('-i', '--input', dest='input', default='decks')
-args.add_argument('-o', '--output', dest='output', default='plover.apkg')
+args.add_argument('-o', '--output', dest='output', default='dist')
 
 options = args.parse_args()
+print(options);
 
-print(options)
+def process():
+    options.input = os.path.abspath(options.input)
+    options.output = os.path.abspath(options.output)
 
-directory = os.path.abspath(options.input)
-files = fnmatch.filter(os.listdir(directory), '*.txt')
+    _, deck_folders, deck_files = next(os.walk(options.input))
 
-anki = Anki()
+    Path(options.output).mkdir(parents=True, exist_ok=True)
 
-print(files)
+    for folder in deck_folders:
+        folder_path = os.path.join(options.input, folder)
+        files = fnmatch.filter(os.listdir(folder_path), "*.txt")
 
-for file in files:
-    entries = open(os.path.join(directory, file)).read().splitlines()
+        if len(files) > 0:
+            generate_deck(folder_path, files, folder)
 
-    for index, entry in enumerate(entries, start=1):
-        if '\t' in entry:
-            anki.generate_note(entry)
-        elif '~~~' in entry:
-            anki.generate_deck(entry)
-        elif not entry or entry.startswith('#'):
-            continue
-        else:
-            raise Exception(f'Unknown Line Type {file}#{index}')
+    for file in fnmatch.filter(deck_files, "*.txt"):
+        generate_deck(options.input, [file], os.path.splitext(file)[0])
 
-anki.generate_package(options.output)
+def generate_deck(input_directory, files, name):
+    output_filepath = os.path.join(options.output, f'{name}.apkg')
+    
+    anki = Anki()
+
+    for file in files:
+        entries = open(os.path.join(input_directory, file)).read().splitlines()
+
+        for index, entry in enumerate(entries, start=1):
+           if '\t' in entry:
+               anki.generate_note(entry)
+           elif '~~~' in entry:
+               anki.generate_deck(entry)
+           elif not entry or entry.startswith('#'):
+               continue
+           else:
+               raise Exception(f'Unknown Line Type {file}#{index}')
+
+    anki.generate_package(output_filepath)
+
+process()
